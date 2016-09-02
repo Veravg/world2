fb6860422aa7d493ebf82c87be71ef32479d202c
Veravg <veravgelder@hotmail.com>
2016-09-02 16:07:35 +0300

    dinoready

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,8 +1,21 @@
 class Dinosaur < ApplicationRecord
   # model -> provide access to application-level data and
   # Encapsulata de Application business login (how data created/displayed/stored and changed)
+    belongs_to :valley
+
     def self.order_by_name
       order(:name)
       # method for: all dinosaurs orderd by name
     end
+
+    def is_baby?
+      age < 3
+    end
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+
+    validates :age, length: { maximum: 5 }, allow_blank: true
+    validates :image_url, length: { maximum: 255 }, allow_blank: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :valleys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dinosaurs
+  root "dinosaurs#index"
+
+  # get "dinosaurs" => "dinosaurs#index"
+  #post "dinosaurs" => "dinosaurs#create"
+  #get "dinosaurs/new" => 'dinosaurs#new', as: :new_dinosaur
+  #get "dinosaurs/:id/edit" => "dinosaurs#edit", as: :edit_dinosaur
+  #get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  #delete "dinosaurs/:id" => "dinosaurs#destroy"
 end
